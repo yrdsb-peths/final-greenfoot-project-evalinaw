d4973677fb700ad3fa21691c7e4bc2f77568b716
evalinaw <117936865+evalinaw@users.noreply.github.com>
2023-01-10 13:22:20 -0500

    Added background, actor, and ground

--- Ground.java
+++ Ground.java
@@ -1,15 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Balloon here.
+ * Write a description of class Ground here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Balloon extends Actor
+public class Ground extends Actor
 {
+    public Ground()
+    {
+        getImage().scale(getImage().getWidth()*6,getImage().getHeight());
+    }
     /**
-     * Act - do whatever the Balloon wants to do. This method is called whenever
+     * Act - do whatever the Ground wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
